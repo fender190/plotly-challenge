de1a8c80c0beb8125d5271709d39b64309692f93
unknown <fender190@hotmail.com>
2020-01-24 19:48:10 -0500

    updated app.js file. Fetching samples, and build bubble chart.

--- Belly_Button_Diversity/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Diversity/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -24,6 +24,43 @@ function buildCharts(sample) {
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+  
+    
+    var url = `/samples/${sample}`;
+
+    d3.json(url).then(data => {
+      console.log(data);
+      // slice first 10 values for pie chart
+      var top_ids = data.otu_ids.slice(0,10);
+      var top_labels = data.otu_labels.slice(0,10);
+      var top_values = data.sample_values.slice(0,10);
+      console.log(top_ids, top_labels, top_values);
+      //plot pie chart
+      var trace = {
+        labels: top_ids,
+        values: top_values,
+        type: "pie",
+        name: "otu_ids",
+        hovertext: top_labels
+        };
+      var layout = {title: "Top 10 Samples"};
+      var data1 = [trace];
+      Plotly.newPlot("pie",data1,layout);
+      //plot bubble chart    
+      var trace2 = {
+        x: data.otu_ids,
+        y: data.sample_values,
+        mode: "markers",
+        marker: { size: data.sample_values,
+        color: data.otu_ids},
+        hovertext: data.otu_labels
+      };
+      var layout2 = {title:"Sample Values by ID"};
+      var data2 = [trace2];
+      Plotly.newPlot("bubble",data2,layout2);
+    });
+
+    
 }
 
 function init() {
